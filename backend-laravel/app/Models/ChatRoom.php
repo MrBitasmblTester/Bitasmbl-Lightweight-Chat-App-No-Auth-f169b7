a96ad3d37b04be34a9a87a113d0d9dfe20a0c39b
MrBitasmblTester <21100302@ibsu.edu.ge>
2026-02-20 11:29:39 +0400

    Add ChatRoom.php

---
+++ backend-laravel/app/Models/ChatRoom.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class ChatRoom extends Model{protected $fillable=['name','slug'];public function messages(){return $this->hasMany(Message::class);}}
