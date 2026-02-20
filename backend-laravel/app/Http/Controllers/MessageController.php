d2cfc9daa5dc481cec4d358a297a11a26f0f338b
MrBitasmblTester <21100302@ibsu.edu.ge>
2026-02-20 11:29:42 +0400

    Add MessageController.php

---
+++ backend-laravel/app/Http/Controllers/MessageController.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class MessageController extends Controller{public function index($roomId){return [];}public function store(Request $r,$roomId){return response()->json([]);}}
