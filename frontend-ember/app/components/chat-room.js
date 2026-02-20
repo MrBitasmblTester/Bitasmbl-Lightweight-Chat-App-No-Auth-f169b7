f734d8209267a6ca32ab922af6eca23fae98a770
MrBitasmblTester <21100302@ibsu.edu.ge>
2026-02-20 11:29:48 +0400

    Add chat-room.js

---
+++ frontend-ember/app/components/chat-room.js
@@ -0,0 +1,4 @@
+import Component from '@glimmer/component';
+import {tracked} from '@glimmer/tracking';
+export default class ChatRoomComponent extends Component{@tracked messages=[];sendMessage(body){}
+}
