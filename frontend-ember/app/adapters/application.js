5700886f289bc5d05562ff39150fad741adb1d48
MrBitasmblTester <21100302@ibsu.edu.ge>
2026-02-20 11:29:45 +0400

    Add application.js

---
+++ frontend-ember/app/adapters/application.js
@@ -0,0 +1,3 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import config from '../config/environment';
+export default class ApplicationAdapter extends JSONAPIAdapter{host=config.APP.apiHost;}
