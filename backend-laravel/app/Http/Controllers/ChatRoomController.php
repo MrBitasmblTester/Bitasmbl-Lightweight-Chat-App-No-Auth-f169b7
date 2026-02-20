02ed302f0a67a285eb00ffbfe1edd79393d41b8d
MrBitasmblTester <21100302@ibsu.edu.ge>
2026-02-20 11:29:34 +0400

    Add ChatRoomController.php

---
+++ backend-laravel/app/Http/Controllers/ChatRoomController.php
@@ -0,0 +1,5 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class ChatRoomController extends Controller{public function index(){return [];}
+public function store(Request $r){return response()->json([]);}}
